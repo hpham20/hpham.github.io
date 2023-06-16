58b254521df4a3c4dcad2c1963cc1f70ec49f02a
Henry <henrypham20@gmail.com>
2023-06-15 21:28:04 -0500

    Fixed responsive design on Skills Page

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,7 +1,10 @@
 export default function PageNotFound() {
     return (
       <div>
-        404: Page Not Found!
+        <div style={{ textAlign: "center", display: "flex", flexDirection:"column", justifyContent: "center", alignItems:"center" }}>
+          <h1 style={{ color: "white" }}>PAGE NOT FOUND!</h1>
+          <img style={{ width: "500px"}} src={require("../images/UnderConstruction.png")} alt="Under construction!" />
+        </div>
       </div>
     )
 }
