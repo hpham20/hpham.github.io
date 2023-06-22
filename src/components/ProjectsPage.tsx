1a3da3d28ff1f9c4a6f04bd9af1c92536da06a9c
Henry <henrypham20@gmail.com>
2023-06-21 21:02:06 -0500

    Experimented with Bootstrap

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,8 +1,26 @@
+import Card from "react-bootstrap/Card"
+
 export default function ProjectsPage() {
     return (
       <div className="projects-page">
         <h1>My Projects</h1>
-        <div className="projects">
+        <div className="projects-grid">
+
+          {/* <div className="project-item project-item-1">
+            RouteRunner
+          </div>
+
+          <div className="project-item project-item-2">
+            WLTH
+          </div> */}
+
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={require("../images/Me_Doodle.jpg")} />
+            <Card.Body>
+              <Card.Title>RouteRunner</Card.Title>
+              <Card.Text>test</Card.Text>
+            </Card.Body>
+          </Card>
 
         </div>
       </div>
