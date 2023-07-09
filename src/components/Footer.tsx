156011711409f9edfecd8e676ba95304fede3d7a
Henry <henrypham20@gmail.com>
2023-07-08 21:54:26 -0500

    Added modal pop-up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ import { FaGithub } from "react-icons/fa"
 export default function Footer() {
     return (
       <div className="footer">
-        <a href="https://www.linkedin.com/in/henry-pham-07/">
+        <a href="https://www.linkedin.com/in/henry-pham-07/" target="_blank">
           <FaLinkedin size={25} className="footer-icon" />
         </a>
-        <a href="https://github.com/hpham20">
+        <a href="https://github.com/hpham20" target="_blank">
         <FaGithub size={25} className="footer-icon" />
         </a>
       </div>
--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+import Carousel from "react-bootstrap/Carousel"
 
 export default function ProjectsPage() {
+    const [show, setShow] = useState<"routerunner" | "wlth" | "close">("close")
+
+    const handleClose = () => setShow("close")
+    const handleShowRouteRunner = () => setShow("routerunner")
+    const handleShowWlth = () => setShow("wlth")
+
     return (
       <div className="projects-page">
         <h1>My Projects</h1>
@@ -15,23 +24,106 @@ export default function ProjectsPage() {
             WLTH
           </div> */}
           <div className="project-item">
-            <Card className="project-card-view" style={{ width: "15rem" }}>
-              <Card.Img variant="top" src={require("../images/projects/routerunner/routerunner_thumbnail.png")} style={{ width: "15rem", borderTopLeftRadius: "2rem", borderTopRightRadius: "2rem" }} />
-              <Card.Body>
-                <Card.Title>RouteRunner</Card.Title>
-                <Card.Text>A mobile app that generates routes for runners</Card.Text>
+            <Card className="project-card-view" onClick={handleShowRouteRunner}>
+              <Card.Img variant="top" src={require("../images/projects/routerunner/routerunner_thumbnail.png")} className="project-card-image" />
+              <Card.Body className="project-card-body" >
+                <Card.Title className="project-card-title">
+                  RouteRunner
+                </Card.Title>
+                <Card.Text className="project-card-text">
+                  A mobile app that generates routes for runners, tracks their goals and stats, and has optional bots for users to race against
+                </Card.Text>
+                {/* <Button className="project-card-btn">Test</Button> */}
               </Card.Body>
             </Card>
           </div>
 
           <div className="project-item">
-            <Card className="project-card-view" style={{ width: "15rem" }} onClick={() => { console.log("TEST") }}>
-              <Card.Img variant="top" src={require("../images/projects/wlth/wlth_thumbnail.png")} style={{ width: "15rem", borderRadius: "1rem" }} />
-              <Card.Body>
-                <Card.Title>WLTH</Card.Title>
-                <Card.Text>Budgeting app built from web</Card.Text>
+            <Card className="project-card-view" onClick={handleShowWlth}>
+              <Card.Img variant="top" src={require("../images/projects/wlth/wlth_thumbnail.png")} className="project-card-image" />
+              <Card.Body className="project-card-body">
+                <Card.Title className="project-card-title">
+                  WLTH ("Wealth")
+                </Card.Title>
+                <Card.Text className="project-card-text">
+                  Budgeting app built for web
+                </Card.Text>
+                {/* <Button className="project-card-btn" onClick={(e) => {e.stopPropagation(); alert("TEST @") }}>Test</Button> */}
               </Card.Body>
             </Card>
+
+            <Modal className="project-modal" show={show === "routerunner"} onHide={handleClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>RouteRunner</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body style={{display: "flex", alignItems:"center"}}>
+                <p style={{flex: 1}}>
+                  This project was created by a team of four as part of a 15-week capstone class.
+                  <br></br><br></br>Tools used: React Native, Node.js, Express.js, PostgreSQL, AWS
+                </p>
+                <Carousel style={{flex: "1"}} interval={null} >
+                  <Carousel.Item>
+                    <img src={require("../images/projects/routerunner/routerunner_1.png")} alt="Home page" />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/routerunner/routerunner_2.png")} alt="Profile/Preference page" />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/routerunner/routerunner_3.png")} alt="Stats page" />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/routerunner/routerunner_4.png")} alt="Activities page" />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/routerunner/routerunner_5.png")} alt="Routes page" />
+                  </Carousel.Item>
+                </Carousel>
+              </Modal.Body>
+              
+              {/* <Modal.Footer></Modal.Footer> */}
+            </Modal>
+
+            <Modal className="project-modal" show={show === "wlth"} onHide={handleClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>WLTH</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body style={{display: "flex", alignItems:"center"}}>
+                <p style={{flex: 1}}>
+                This project was created by a team of seven using the agile methodology.
+                  <br></br><br></br>
+                  Tools used: React, Node.js, Express.js, PostgreSQL
+                </p>
+                <Carousel style={{flex: "1"}} interval={null} >
+                  <Carousel.Item>
+                    <img src={require("../images/projects/wlth/wlth_1.png")} />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/wlth/wlth_2.png")} />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/wlth/wlth_3.png")} />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/wlth/wlth_4.png")} />
+                  </Carousel.Item>
+
+                  <Carousel.Item>
+                    <img src={require("../images/projects/wlth/wlth_5.png")} />
+                  </Carousel.Item>
+                </Carousel>
+              </Modal.Body>
+              
+              {/* <Modal.Footer></Modal.Footer> */}
+            </Modal>
           </div>
         </div>
       </div>
